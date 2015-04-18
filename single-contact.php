758dd97b35931d689a69cf4f549d4379ca314543
rmkristiansen <post@roykristiansen.com>
2015-04-18 18:19:14 +0200

    fix footer issue on contact page

--- single-contact.php
+++ single-contact.php
@@ -7,67 +7,40 @@ get_header(); ?>
 
 
 <?php get_template_part( 'template-content-header-normal'); ?> 
-            
 
 
-        <div id="content-no-sidebar" >
-            <div id="content-padding">
+<div id="content-no-sidebar" >
+    <div id="content-padding">
 
-<?php get_template_part( 'template-content-header-big'); ?> 
+    <?php get_template_part( 'template-content-header-big'); ?> 
 
-    <div id="content-wrapper">
+        <?php $items = new WP_Query( array( 'post_type' => 'contact') );
+        if( $items->have_posts() ) : ?>
 
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div id="normal-content" class="col-xs-12">
+        <div class="contact-first-column col-xs-12 col-sm-6 col-md-4">
+            <?php echo apply_filters('the_content',get_post_meta(get_the_ID(), 'first_column', true)); ?>
+        </div> <!-- .contact-first-column -->
 
+        <div class="contact-second-column col-xs-12 col-sm-6 col-md-4">
+            <?php echo apply_filters('the_content',get_post_meta(get_the_ID(), 'second_column', true)); ?>
+        </div> <!-- .contact-second-column -->
 
-            <?php $items = new WP_Query( array( 'post_type' => 'contact') );
-            if( $items->have_posts() ) : ?>
-                <div class="contact-first-column col-xs-12 col-sm-6 col-md-4">
+    <?php endif; wp_reset_query(); // Reset the loop. ?>  
 
-                        <?php
+    <div class="contact-form-widget col-xs-12 col-md-4">
+        <?php echo do_shortcode(get_post_meta(get_the_ID(), 'contact_form_shortcode', true)) ?>
+    </div> <!-- .contact-form-widget -->
 
-
-                        echo apply_filters('the_content',get_post_meta(get_the_ID(), 'first_column', true));
-
-                        ?>
-
-
-                </div>
-                <div class="contact-second-column col-xs-12 col-sm-6 col-md-4">
-                    <?php echo apply_filters('the_content',get_post_meta(get_the_ID(), 'second_column', true)); ?>
-                </div>
-
-
-                <?php wp_reset_query(); // Reset the loop.
-            endif; ?>
-            <div class="contact-form-widget col-xs-12 col-md-4">
-
-                <?php echo do_shortcode(get_post_meta(get_the_ID(), 'contact_form_shortcode', true)) ?>
-
-            </div>
+    </div> <!-- .content-padding -->
+</div> <!-- #content-no-sidebar -->
 
 
 
-
-
-
-
-            </div>
-        </div>
-
-        <div id="no-sidebar" >
-          a
-        </div> <!-- #sidebar -->
-
-    </div> <!-- .content -->
-    
-
-
+</div> <!-- #content -->
 
     <div class="push"></div> <!-- .push must be inside #wrapper -->
-
 </div> <!-- #wrapper -->
+
 
 <?php
 get_footer();
