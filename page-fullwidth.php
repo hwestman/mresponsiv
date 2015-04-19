0cdc11177dbc3b017eb1bd5d6564cf8eb6f6b3ee
Hallvard Westman <hallvard@westman.no>
2015-04-19 21:28:48 +0200

    fixed gallery

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -6,19 +6,20 @@ get_header(); ?>
 
 
 <?php  get_template_part( 'template-content-header-normal'); ?>
+        <div id="full-width-content">
+            <div id="content-padding">
 
-    <div id="content-sidebar" class="content-left" >
-        <div id="content-padding">
+                <?php
+                while ( have_posts() ) : the_post();
+                    get_template_part( 'content', get_post_format() );
+                endwhile;
+                ?>
+                <?php wp_reset_query(); ?>
 
-            <?php
-            while ( have_posts() ) : the_post();
-                get_template_part( 'content', get_post_format() );
-            endwhile;
-            ?>
-            <?php wp_reset_query(); ?>
-
-        </div> <!-- #content-padding -->
-    </div> <!-- #content-sidebar -->
+            </div> <!-- #content-padding -->
+        </div>
+    </div>
+    <div class="push"></div> <!-- .push must be inside #wrapper -->
 
 </div> <!-- #wrapper -->
 
