74fd4e74d8adcfeed3430b60d031d9e51aed3a52
Hallvard Westman <hallvard@westman.no>
2015-04-20 10:41:15 +0200

    Update template-service.php
    added content div for tables to take effect

--- template-service.php
+++ template-service.php
@@ -8,9 +8,10 @@ $post = get_post();
 ?>
 <div id="content-sidebar" class="content-left" >
     <div id="content-padding">
-
+        <div class="post-content">
             <h1 class="post-heading"><?php echo $post->post_title ?></h1>
             <?php echo $post->post_content; ?>
+        </div>
 
     </div>
 </div>
