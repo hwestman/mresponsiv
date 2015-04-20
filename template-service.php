e51c8f8953fd2be3f3c54212d034039c278bc8d4
Hallvard Westman <hallvard@westman.no>
2015-04-20 10:49:30 +0200

    Update template-service.php
    added some stuff

--- template-service.php
+++ template-service.php
@@ -9,8 +9,10 @@ $post = get_post();
 <div id="content-sidebar" class="content-left" >
     <div id="content-padding">
         <div class="post-content">
-            <h1 class="post-heading"><?php echo $post->post_title ?></h1>
-            <?php echo $post->post_content; ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h1 class="post-heading"><?php echo $post->post_title ?></h1>
+                <?php echo do_shortcode( $post->post_content ); ?>
+            </article>
         </div>
 
     </div>
