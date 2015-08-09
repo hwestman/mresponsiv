56b762dbbfd34a0dc8a51c54578c08bcbe1d334d
Hallvard Westman <hallvard@westman.no>
2015-08-09 21:23:46 +0200

    removed </p>

--- template-frontpage-services.php
+++ template-frontpage-services.php
@@ -16,7 +16,7 @@
 	<?php if( $items->have_posts() ) : ?>
  	<?php while( $items->have_posts() ) : $items->the_post(); ?>
 
-	<?php $services_url = get_post_meta($post->ID, 'collaborator_url', true);?></p>
+	<?php $services_url = get_post_meta($post->ID, 'collaborator_url', true);?>
 	<?php $services_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'square'); ?>
 
 
