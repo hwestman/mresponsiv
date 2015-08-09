c2b0a6a7ff29b41cf86427f726880dbff38fd54c
rmKristiansen <post@roykristiansen.com>
2015-08-09 20:05:45 +0200

    Fixed 404 page

--- 404.php
+++ 404.php
@@ -1,32 +1,40 @@
-<?php
-/**
- * The template for displaying 404 pages (Not Found)
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
 
+<?php
 get_header(); ?>
 
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+<?php  get_template_part( 'template-content-header-normal'); ?> 
 
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'twentyfourteen' ); ?></h1>
-			</header>
+<div id="content-sidebar" class="content-left" >
+  <div id="content-padding">
+	
+	<h1 class="success-text-heading">Har du gått deg bort?</h1>
+	<p>Vi fant dessverre ikke siden du leter etter (404). <br>For å gå tilbake til forsiden, kan du klikke på linken under.</p>
+	<a  href="<?php echo site_url(); ?>" class="not-found-button">&#8592; Tilbake til forsiden</a>
 
-			<div class="page-content">
-				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfourteen' ); ?></p>
+  </div> <!-- #content-padding -->
+</div> <!-- #content-sidebar -->
 
-				<?php get_search_form(); ?>
-			</div><!-- .page-content -->
+        <div id="sidebar" class="sidebar-right" >
+          <div id="sidebar-padding">
+            <?php if (function_exists('dynamic_sidebar') && dynamic_sidebar('m_res_page')) : else : ?>
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
+              <!-- Do nothing if dont exist-->
+
+              <?php endif; ?>
+          </div>
+        </div> <!-- #sidebar -->
+
+</div>
+
+<div id="sidebar-background" class="sidebar-background-right">
+  
+</div> <!-- #sidebar-background -->
+
+
+    <div class="push"></div> <!-- .push must be inside #wrapper -->
+
+</div> <!-- #wrapper -->
 
 <?php
-get_sidebar( 'content' );
-get_sidebar();
 get_footer();
